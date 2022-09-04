6a2a29371b4e832fac5fbead4df5309e4e9d3262
darmyn <81786165+darmyn@users.noreply.github.com>
2022-09-04 01:16:21 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.youtube.com/watch?v=Iuk1znROeKg"
-result = requests.get(url).text
-doc = BeautifulSoup(result, "html.parser")
-print(doc.find_all('ytd-metadata-row-container-renderer', {"class": "style-scope ytd-watch-metadata"}))
